3586ba8a4bd2f5319978c1bf03c083c9faab5e20
Clark Lee <marsbound@protonmail.com>
2019-06-25 07:29:27 -0500

    add minimax + refactor board's data structures

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io::{self, Write};
 use std::{fmt, collections, error};
-use dialoguer::Input;
+use dialoguer::{Input, theme::ColorfulTheme, Select};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Player {
     Human,
-    AI,
+    COMP,
     None(u8)
 }
 
@@ -13,7 +13,7 @@ impl fmt::Display for Player {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Player::Human => write!(f, "o"),
-            Player::AI => write!(f, "x"),
+            Player::COMP => write!(f, "x"),
             Player::None(x) => write!(f, "{}", x),
         }
     }
@@ -36,55 +36,54 @@ impl error::Error for BoardPlacementError {
     }
 }
 
+#[derive(Clone)]
 struct Board {
-    positions: [[Player; 3]; 3],
-    position_alias: collections::HashMap<u8, (usize, usize)>
+    positions: collections::HashMap<u8, Player>,
+    available_positions: collections::HashMap<u8, Player>
 }
 
 impl Board {
     fn new() -> Self {
-        let positions: [[Player; 3]; 3] = 
-            [[Player::None(1), Player::None(4), Player::None(7)], 
-            [Player::None(2), Player::None(5), Player::None(8)], 
-            [Player::None(3), Player::None(6), Player::None(9)]];
-
-        let mut position_alias = collections::HashMap::new();
-        position_alias.insert(1, (0, 0));
-        position_alias.insert(2, (1, 0));
-        position_alias.insert(3, (2, 0));
-        position_alias.insert(4, (0, 1));
-        position_alias.insert(5, (1, 1));
-        position_alias.insert(6, (2, 1));
-        position_alias.insert(7, (0, 2));
-        position_alias.insert(8, (1, 2));
-        position_alias.insert(9, (2, 2));
-        
+        let mut positions = collections::HashMap::new();
+        for i in 1..=9 {
+            positions.insert(i, Player::None(i));
+        }
+        let available_positions = positions.clone();
         Board {
             positions,
-            position_alias
+            available_positions
         }
     }
-
-    fn visualize(&self, stdout: &io::Stdout) -> Result<(), io::Error> {
+    fn draw(&self) -> Result<(), io::Error> {
+        let stdout = io::stdout();
         let mut handle = io::BufWriter::new(stdout.lock());
-
         writeln!(handle)?;
-        writeln!(handle, "{} | {} | {}", self.positions[0][0], self.positions[1][0], self.positions[2][0])?;
+        writeln!(handle, "{} | {} | {}", self.positions.get(&1).unwrap(), 
+            self.positions.get(&2).unwrap(), self.positions.get(&3).unwrap())?;
         writeln!(handle, "{} {} {} {} {}", "-", "+", "-", "+", "-")?;
-        writeln!(handle, "{} | {} | {}", self.positions[0][1], self.positions[1][1], self.positions[2][1])?;
+        writeln!(handle, "{} | {} | {}", self.positions.get(&4).unwrap(), 
+            self.positions.get(&5).unwrap(), self.positions.get(&6).unwrap())?;
         writeln!(handle, "{} {} {} {} {}", "-", "+", "-", "+", "-")?;
-        writeln!(handle, "{} | {} | {}", self.positions[0][2], self.positions[1][2], self.positions[2][2])?;
+        writeln!(handle, "{} | {} | {}", self.positions.get(&7).unwrap(), 
+            self.positions.get(&8).unwrap(), self.positions.get(&9).unwrap())?;
         writeln!(handle)?;
-
         Ok(())
     }
 
-    fn place_move_with_alias(&mut self, alias: u8, player: Player) -> Result<Option<Player>, BoardPlacementError> {
-        match self.position_alias.get(&alias) {
-            Some(&(row, col)) => {
-                match self.place_move(row, col, player) {
-                    Ok(player) => Ok(player),
-                    Err(e) => Err(e)
+    fn place_move(&mut self, at: u8, player: Player) -> Result<(), BoardPlacementError> {
+        match self.positions.get(&at) {
+            Some(position) => {
+                match position {
+                    Player::None(_) => {
+                        *self.positions.get_mut(&at).unwrap() = player;
+                        self.available_positions.remove(&at);
+                        Ok(())
+                    },
+                    _ => {
+                        Err(BoardPlacementError {
+                            message: String::from("Invalid move! Position is already taken.")
+                        })
+                    }
                 }
             },
             _ => {
@@ -95,116 +94,126 @@ impl Board {
         }
     }
 
-    // Credits to "Hardwareguy" for the algorithm
-    // Source: https://stackoverflow.com/a/1056352
-    fn place_move(&mut self, x: usize, y: usize, player: Player) -> Result<Option<Player>, BoardPlacementError> {
-        if let Player::None(_) = self.positions[x][y] {
-            self.positions[x][y] = player;
-        } else {
-            return Err(BoardPlacementError {
-                message: String::from("Invalid move! Position is already taken.")
-            });
-        }
-
-        let n = 3;
-        
-        // Check columns
-        for i in 0..n {
-            if self.positions[x][i] != player {
-                break;
-            }
-            if i == n-1 {
-                return Ok(Some(player))
-            }
-        }
-
-        // Check rows
-        for i in 0..n {
-            if self.positions[i][y] != player {
-                break;
-            }
-            if i == n-1 {
-                return Ok(Some(player))
-            }
-        }
-
-        // Check diagonal
-        if x == y {
-            for i in 0..n {
-                if self.positions[i][i] != player {
-                    break;
-                }
-                if i == n-1 {
-                    return Ok(Some(player))
+    fn evaluate(&self) -> i8 {
+        for (combo, step) in [([1, 4, 7], 1), ([1, 2, 3], 3), ([1, 5, 9], 0), ([3, 5, 7], 0)].iter() {
+            for i in 0..3 {
+                let step = (step*i) as u8;
+                let val1 = self.positions.get(&(combo[0]+step)).unwrap();
+                let val2 = self.positions.get(&(combo[1]+step)).unwrap();
+                let val3 = self.positions.get(&(combo[2]+step)).unwrap();
+                if val1 == val2 && val2 == val3 {
+                    if val1 == &Player::COMP {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
                 }
             }
         }
-
-        // Check anti-diagonal
-        if x + y == n-1 {
-            for i in 0..n {
-                if self.positions[i][(n-1)-i] != player {
-                    break;
-                }
-                if i == n-1 {
-                    return Ok(Some(player))
-                }
-            }
-        }
-
-        Ok(None)
+        0
     }
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let stdout = io::stdout();
     let mut board = Board::new();
-    let mut outcome: Option<Player>;
+    println!("\nXO: Tic-tac-toe\n");
+    let selections = [
+        "Yes",
+        "No"
+    ];
+    let selection = Select::with_theme(&ColorfulTheme::default()).with_prompt("Do you want to go first?")
+        .default(0).items(&selections[..]).interact()?;
+    let order;
+    if selections[selection] == "Yes" {
+        order = 1;
+    } else {
+        order = 0;
+    }
 
-    board.visualize(&stdout)?;
-    for i in 0..9 {
-        if i % 2 == 0 {
+    board.draw()?;
+
+    for i in (1..=9).rev() {
+        if i % 2 == order {
             loop {
-                let choice = Input::<u8>::new().with_prompt("Your turn").interact()?;
-                match board.place_move_with_alias(choice, Player::Human) {
-                    Ok(player) => {
-                        outcome = player;
+                let choice = Input::<u8>::new().with_prompt("Your move").interact()?;
+                match board.place_move(choice, Player::Human) {
+                    Ok(_) => {
                         break;
                     },
                     Err(e) => println!("{}", e)
                 }
             }
         } else {
-            let choice = Input::<u8>::new().with_prompt("AI's turn").interact()?;
-            outcome = board.place_move_with_alias(choice, Player::AI)?;
+            let optimal_move;
+            // Hardcoded strategic placements for higher depths to save computation.
+            if i == 9 {
+                optimal_move = OptimalMove { position: 1, score: 0 };
+            } else if i == 7 {
+                if !board.available_positions.contains_key(&5) {
+                    optimal_move = OptimalMove { position: 9, score: 0 };
+                } else {
+                    optimal_move = minimax(&board, i, Player::COMP);
+                }
+            } else {
+                optimal_move = minimax(&board, i, Player::COMP);
+            }
+            println!("Computer's move: {}", optimal_move.position);
+            board.place_move(optimal_move.position as u8, Player::COMP)?;
         }
-
-        match outcome {
-            Some(player) => {
-                match player {
-                    Player::AI => {
-                        board.visualize(&stdout)?;
-                        println!("* AI wins! *\n", );
-                        break;
-                    },
-                    Player::Human => {
-                        board.visualize(&stdout)?;
-                        println!("* You win! *\n", );
-                        break;
-                    },
-                    _ => ()
-                }
+        board.draw()?;
+        let evaluation = board.evaluate();
+        if evaluation == 1 {
+            if i % 2 == order {
+                println!("* You win! *\n");
+                break;
+            } else {
+                println!("* Computer wins! *\n");
+                break;
             }
-            _ => {
-                println!("\n- Turn {} -", i+1);
-                board.visualize(&stdout)?;
-                if i == 8 {
-                    println!("Draw");
-                }
-            }
+        } else if evaluation == 0 && i == 1 {
+            println!("* Tie *\n");
+            break;
         }
     }
-
     Ok(())
 }
 
+struct OptimalMove {
+    position: i8,
+    score: i8
+}
+
+fn minimax(board: &Board, depth: i8, player: Player) -> OptimalMove {
+    let opponent;
+    let mut optimal;
+    if player == Player::COMP {
+        opponent = Player::Human;
+        optimal = OptimalMove { position: -1, score: -50 };
+    } else {
+        opponent = Player::COMP;
+        optimal = OptimalMove { position: -1, score: 50 };
+    }
+
+    let score = board.evaluate();
+    if depth == 0 || score == 1 || score == -1 {
+        return OptimalMove { position: -1, score };
+    }
+
+    for (k, _) in board.available_positions.iter() {
+        let mut new_board = board.clone();
+        let _ = new_board.place_move(*k, player);
+        let mut current_optimal = minimax(&new_board, depth-1, opponent);
+        current_optimal.position = *k as i8;
+
+        if player == Player::COMP {
+            if current_optimal.score > optimal.score {
+                optimal = current_optimal;
+            }
+        } else {
+            if current_optimal.score < optimal.score {
+                optimal = current_optimal;
+            }
+        }
+    }
+    optimal
+}
